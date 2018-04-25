d97f0238dd626a75d4085a618fc701aa6d410d9d
wantony <wantony1314@gmail.com>
2018-04-26 00:14:03 +0800

    1

---
+++ App_Code/wTools.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// wTools 的摘要描述
+/// </summary>
+public class wTools
+{
+    public wTools()
+    {
+        //
+        // TODO: 在這裡新增建構函式邏輯
+        //
+    }
+}
